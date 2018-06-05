1401bc51b96be6e0b95956b0e6473a237e4b0936
phiamo <phiamo@googlemail.com>
2018-06-05 12:33:01 +0200

    tab to space

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
@@ -22,69 +22,69 @@ import android.widget.RelativeLayout;
 import android.widget.VideoView;
 
 public class SimpleVideoStream extends Activity implements
-	MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
-	MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
-	private String TAG = getClass().getSimpleName();
-	private VideoView mVideoView = null;
-	private MediaPlayer mMediaPlayer = null;
-	private MediaController mMediaController = null;
-	private ProgressBar mProgressBar = null;
-	private String mVideoUrl;
-	private Boolean mShouldAutoClose = true;
+    MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+    MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
+    private String TAG = getClass().getSimpleName();
+    private VideoView mVideoView = null;
+    private MediaPlayer mMediaPlayer = null;
+    private MediaController mMediaController = null;
+    private ProgressBar mProgressBar = null;
+    private String mVideoUrl;
+    private Boolean mShouldAutoClose = true;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		Bundle b = getIntent().getExtras();
-		mVideoUrl = b.getString("mediaUrl");
-		mShouldAutoClose = b.getBoolean("shouldAutoClose");
-		mShouldAutoClose = mShouldAutoClose == null ? true : mShouldAutoClose;
+        Bundle b = getIntent().getExtras();
+        mVideoUrl = b.getString("mediaUrl");
+        mShouldAutoClose = b.getBoolean("shouldAutoClose");
+        mShouldAutoClose = mShouldAutoClose == null ? true : mShouldAutoClose;
 
-		RelativeLayout relLayout = new RelativeLayout(this);
-		relLayout.setBackgroundColor(Color.BLACK);
-		RelativeLayout.LayoutParams relLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-		relLayoutParam.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-		mVideoView = new VideoView(this);
-		mVideoView.setLayoutParams(relLayoutParam);
+        RelativeLayout relLayout = new RelativeLayout(this);
+        relLayout.setBackgroundColor(Color.BLACK);
+        RelativeLayout.LayoutParams relLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        relLayoutParam.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        mVideoView = new VideoView(this);
+        mVideoView.setLayoutParams(relLayoutParam);
 
         Log.d(TAG, "Bundle: " + b.toString());
         int position = b.getInt("position") * 1000;
 
-		relLayout.addView(mVideoView);
+        relLayout.addView(mVideoView);
 
-		// Create progress throbber
-		mProgressBar = new ProgressBar(this);
-		mProgressBar.setIndeterminate(true);
-		// Center the progress bar
-		RelativeLayout.LayoutParams pblp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-		pblp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-		mProgressBar.setLayoutParams(pblp);
-		// Add progress throbber to view
-		relLayout.addView(mProgressBar);
-		mProgressBar.bringToFront();
+        // Create progress throbber
+        mProgressBar = new ProgressBar(this);
+        mProgressBar.setIndeterminate(true);
+        // Center the progress bar
+        RelativeLayout.LayoutParams pblp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        pblp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        mProgressBar.setLayoutParams(pblp);
+        // Add progress throbber to view
+        relLayout.addView(mProgressBar);
+        mProgressBar.bringToFront();
 
-		setOrientation(b.getString("orientation"));
+        setOrientation(b.getString("orientation"));
 
-		setContentView(relLayout, relLayoutParam);
+        setContentView(relLayout, relLayoutParam);
 
-		play(position);
-	}
+        play(position);
+    }
 
-	private void play(int position) {
-		mProgressBar.setVisibility(View.VISIBLE);
-		Uri videoUri = Uri.parse(mVideoUrl);
-		try {
-			mVideoView.setOnCompletionListener(this);
-			mVideoView.setOnPreparedListener(this);
-			mVideoView.setOnErrorListener(this);
-			mVideoView.setVideoURI(videoUri);
-			mMediaController = new MediaController(this);
-			mMediaController.setAnchorView(mVideoView);
-			mMediaController.setMediaPlayer(mVideoView);
-			mVideoView.setMediaController(mMediaController);
+    private void play(int position) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        Uri videoUri = Uri.parse(mVideoUrl);
+        try {
+            mVideoView.setOnCompletionListener(this);
+            mVideoView.setOnPreparedListener(this);
+            mVideoView.setOnErrorListener(this);
+            mVideoView.setVideoURI(videoUri);
+            mMediaController = new MediaController(this);
+            mMediaController.setAnchorView(mVideoView);
+            mMediaController.setMediaPlayer(mVideoView);
+            mVideoView.setMediaController(mMediaController);
             Log.d(TAG, "Seeking to " + position);
             mVideoView.seekTo(position);
             final int pos = position;
@@ -100,122 +100,122 @@ public class SimpleVideoStream extends Activity implements
                     }
                 });
             }
-		} catch (Throwable t) {
-			Log.d(TAG, t.toString());
-		}
-	}
+        } catch (Throwable t) {
+            Log.d(TAG, t.toString());
+        }
+    }
 
-	private void setOrientation(String orientation) {
-		if ("landscape".equals(orientation)) {
-			this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		}else if("portrait".equals(orientation)) {
-			this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
-	}
+    private void setOrientation(String orientation) {
+        if ("landscape".equals(orientation)) {
+            this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }else if("portrait".equals(orientation)) {
+            this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
 
-	private Runnable checkIfPlaying = new Runnable() {
-		@Override
-		public void run() {
-			if (mVideoView.getCurrentPosition() > 0) {
-				// Video is not at the very beginning anymore.
-				// Hide the progress bar.
-				mProgressBar.setVisibility(View.GONE);
-			} else {
-				// Video is still at the very beginning.
-				// Check again after a small amount of time.
-				mVideoView.postDelayed(checkIfPlaying, 100);
-			}
-		}
-	};
+    private Runnable checkIfPlaying = new Runnable() {
+        @Override
+        public void run() {
+            if (mVideoView.getCurrentPosition() > 0) {
+                // Video is not at the very beginning anymore.
+                // Hide the progress bar.
+                mProgressBar.setVisibility(View.GONE);
+            } else {
+                // Video is still at the very beginning.
+                // Check again after a small amount of time.
+                mVideoView.postDelayed(checkIfPlaying, 100);
+            }
+        }
+    };
 
-	@Override
-	public void onPrepared(MediaPlayer mp) {
-		Log.d(TAG, "Stream is prepared");
-		mMediaPlayer = mp;
-		mMediaPlayer.setOnBufferingUpdateListener(this);
-		mVideoView.requestFocus();
-		mVideoView.start();
-		mVideoView.postDelayed(checkIfPlaying, 0);
-	}
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(TAG, "Stream is prepared");
+        mMediaPlayer = mp;
+        mMediaPlayer.setOnBufferingUpdateListener(this);
+        mVideoView.requestFocus();
+        mVideoView.start();
+        mVideoView.postDelayed(checkIfPlaying, 0);
+    }
 
-	private void pause() {
-		Log.d(TAG, "Pausing video.");
-		mVideoView.pause();
-	}
+    private void pause() {
+        Log.d(TAG, "Pausing video.");
+        mVideoView.pause();
+    }
 
-	private void stop() {
-		Log.d(TAG, "Stopping video.");
-		mVideoView.stopPlayback();
-	}
+    private void stop() {
+        Log.d(TAG, "Stopping video.");
+        mVideoView.stopPlayback();
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		stop();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stop();
+    }
 
-	private void wrapItUp(int resultCode, String message) {
-		Intent intent = new Intent();
-		intent.putExtra("message", message);
-		int position = mVideoView.getCurrentPosition();
-		intent.putExtra("position", Integer.toString(position));
-		setResult(resultCode, intent);
-		finish();
-	}
+    private void wrapItUp(int resultCode, String message) {
+        Intent intent = new Intent();
+        intent.putExtra("message", message);
+        int position = mVideoView.getCurrentPosition();
+        intent.putExtra("position", Integer.toString(position));
+        setResult(resultCode, intent);
+        finish();
+    }
 
-	public void onCompletion(MediaPlayer mp) {
-		stop();
-		if (mShouldAutoClose) {
-			wrapItUp(RESULT_OK, null);
-		}
-	}
+    public void onCompletion(MediaPlayer mp) {
+        stop();
+        if (mShouldAutoClose) {
+            wrapItUp(RESULT_OK, null);
+        }
+    }
 
-	public boolean onError(MediaPlayer mp, int what, int extra) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("MediaPlayer Error: ");
-		switch (what) {
-			case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
-				sb.append("Not Valid for Progressive Playback");
-				break;
-			case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
-				sb.append("Server Died");
-				break;
-			case MediaPlayer.MEDIA_ERROR_UNKNOWN:
-				sb.append("Unknown");
-				break;
-			default:
-				sb.append(" Non standard (");
-				sb.append(what);
-				sb.append(")");
-		}
-		sb.append(" (" + what + ") ");
-		sb.append(extra);
-		Log.e(TAG, sb.toString());
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("MediaPlayer Error: ");
+        switch (what) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                sb.append("Not Valid for Progressive Playback");
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                sb.append("Server Died");
+                break;
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                sb.append("Unknown");
+                break;
+            default:
+                sb.append(" Non standard (");
+                sb.append(what);
+                sb.append(")");
+        }
+        sb.append(" (" + what + ") ");
+        sb.append(extra);
+        Log.e(TAG, sb.toString());
 
-		wrapItUp(RESULT_CANCELED, sb.toString());
-		return true;
-	}
+        wrapItUp(RESULT_CANCELED, sb.toString());
+        return true;
+    }
 
-	public void onBufferingUpdate(MediaPlayer mp, int percent) {
-		Log.d(TAG, "onBufferingUpdate : " + percent + "%");
-	}
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        Log.d(TAG, "onBufferingUpdate : " + percent + "%");
+    }
 
-	@Override
-	public void onBackPressed() {
-		// If we're leaving, let's finish the activity
-		wrapItUp(RESULT_OK, null);
-	}
+    @Override
+    public void onBackPressed() {
+        // If we're leaving, let's finish the activity
+        wrapItUp(RESULT_OK, null);
+    }
 
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		// The screen size changed or the orientation changed... don't restart the activity
-		super.onConfigurationChanged(newConfig);
-	}
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // The screen size changed or the orientation changed... don't restart the activity
+        super.onConfigurationChanged(newConfig);
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (mMediaController != null)
-			mMediaController.show();
-		return false;
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mMediaController != null)
+            mMediaController.show();
+        return false;
+    }
 }
